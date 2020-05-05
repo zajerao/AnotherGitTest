c6c356cf7cd343763596339b30897260e887d9fc
zajerao <37913127+zajerao@users.noreply.github.com>
2020-05-05 18:10:25 +0100

    Initial Commit

---
+++ AnotherGitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnotherGitTest
+//
+//  Created by imac ssd on 05/05/2020.
+//  Copyright © 2020 imac ssd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
